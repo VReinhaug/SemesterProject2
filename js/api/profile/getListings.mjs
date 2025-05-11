2043b5d20153bd9a687656337ea1c285b613239d
VReinhaug <v.reinhaug@gmail.com>
2025-05-11 15:01:27 +0200

    fixed constants bug

--- js/api/profile/getListings.mjs
+++ js/api/profile/getListings.mjs
@@ -1,8 +1,10 @@
 import { authFetch } from "../fetch.mjs";
-import { API_BASE_URL, API_PROFILE } from "../constants.mjs";
+import { API_BASE_URL, API_SINGLE_PROFILE } from "../constants.mjs";
 
 export async function getListings(name) {
-  const res = await authFetch(`${API_BASE_URL}${API_PROFILE}${name}/listings`);
+  const res = await authFetch(
+    `${API_BASE_URL}${API_SINGLE_PROFILE(name)}/listings`,
+  );
   const { data } = await res.json();
   return data;
 }
